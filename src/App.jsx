7fabd48337cd9e6f39e072e2777c8fe790d004b9
Mark Lawson <mark@walkonmars.co.uk>
2022-06-16 12:15:45 +0100

    removed todo list

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,13 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
 
-  const addTodo = () => {
-    if (todo !== "") {
-      setTodos([...todos, todo]);
-      setTodo("");
-    }
-  };
-
-  const deleteTodo = (text) => {
-    const newTodos = todos.filter((todo) => {
-      return todo !== text;
-    });
-    setTodos(newTodos);
-  };
-
   return (
-    <div className="App">
-      <h1>My Todos</h1>
-      <Todo todo={todo} setTodo={setTodo} addTodo={addTodo} />
-      <TodoList todos={todos} remove={deleteTodo} />
-    </div>
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add todo</button>
+      <button>clear</button>
+    </>
   );
 };
 
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,19 +2,11 @@ import React from "react";
 
 const Todo = ({todo, setTodo, addTodo}) => {
   return (
-    <div className="input-wrapper">
-      <input
-        type="text"
-        name="todo"
-        value={todo}
-        placeholder="Add your task here"
-        onChange={(e) => {
-          setTodo(e.target.value);
-        }}
-      />
-      <button className="add-button" onClick={addTodo}>
-        Add
-      </button>
+    <div>
+      <label>
+        <input type="checkbox" checked={todo.complete} />
+        {todo.name}
+      </label>
     </div>
   );
 };
